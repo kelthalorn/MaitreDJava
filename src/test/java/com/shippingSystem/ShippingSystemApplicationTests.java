19e3a574996cb73996b5bc0aa2f4d393faee15fc
THCO-Octo <thibaut.colin-ext@iadinternational.com>
2021-09-29 11:56:19 +0200

    adding default project files

---
+++ src/test/java/com/shippingSystem/ShippingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shippingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
